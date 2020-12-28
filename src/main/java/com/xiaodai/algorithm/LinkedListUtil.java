1e8e5f7eaf9cac446fdbd1d8e4968c8d27d8693e
ningfeng <ningfeng@dtstack.com>
2020-12-28 17:00:40 +0800

    custom linkedList

--- src/main/java/com/xiaodai/algorithm/LinkedListUtil.java
+++ src/main/java/com/xiaodai/algorithm/LinkedListUtil.java
@@ -7,14 +7,6 @@ package com.xiaodai.algorithm;
  */
 public class LinkedListUtil {
 
-    /**
-     * 传入头节点，翻转单项链表
-     * @param head
-     * @return
-     */
-//    public static Node reverseLinkedList(Node head) {
-//
-//    }
 
     /**
      * 链表的节点，可实现成泛型
@@ -41,5 +33,101 @@ public class LinkedListUtil {
         }
     }
 
+    /**
+     * 传入头节点，翻转单项链表
+     *
+     * @param head
+     * @return
+     */
+    public static Node reverseLinkedList(Node head) {
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    /**
+     * 移除链表中等于值的节点
+     *
+     * @param head
+     * @param num
+     * @return
+     */
+    public static Node removeValue(Node head, int num) {
+
+        // 从链表的头开始，舍弃掉开头的且连续的等于num的节点
+        while (head != null) {
+            if (head.value != num) {
+                break;
+            }
+            head = head.next;
+        }
+
+        // head来到 第一个不需要删的位置
+        Node pre = head;
+        Node cur = head;
+
+        // 快慢指针
+        while (cur != null) {
+            if (cur.value == num) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        int len = 10;
+        int value = 100;
+        // 随机生成一个链表
+        Node head = generateRandomLinkedList(len, value);
+
+//        while (head != null) {
+//            System.out.println(head.value);
+//            head = head.next;
+//        }
+
+        System.out.println("翻转后===");
+        Node reverseHead = reverseLinkedList(head);
+
+        while (reverseHead != null) {
+            System.out.println(reverseHead.value);
+            reverseHead = reverseHead.next;
+        }
+
+    }
+
+    /**
+     * 随机生成一个链表
+     *
+     * @param len
+     * @param value
+     * @return
+     */
+    public static Node generateRandomLinkedList(int len, int value) {
+        int size = (int) (Math.random() * (len + 1));
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        Node head = new Node((int) (Math.random() * (value + 1)));
+        Node pre = head;
+        while (size != 0) {
+            Node cur = new Node((int) (Math.random() * (value + 1)));
+            pre.next = cur;
+            pre = cur;
+            size--;
+        }
+        return head;
+    }
 
 }
