84ad4975823f5d1ec57a12839e325dd0d6080ac7
dai <ningfeng@dtstack.com>
2021-03-16 13:49:40 +0800

    打印两个链表的公共部分

--- src/main/java/com/xiaodai/algorithm/LinkedListUtil.java
+++ src/main/java/com/xiaodai/algorithm/LinkedListUtil.java
@@ -118,6 +118,29 @@ public class LinkedListUtil {
         return head;
     }
 
+    /**
+     * 打印两个链表的公共部分
+     * @param head1
+     * @param head2
+     */
+    public void printCommonPart(Node head1, Node head2) {
+
+        System.out.println("Common Part: ");
+
+        while (head1 != null && head2 != null) {
+            if(head1.value < head2.value) {
+                head1 = head1.next;
+            } else if (head1.value > head2.value) {
+                head2 = head2.next;
+            } else {
+                System.out.println(head1.value);
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
         int len = 10;
