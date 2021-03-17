04a88122ee7c5073c0105731dd2b057b54bffc37
dai <ningfeng@dtstack.com>
2021-03-17 20:52:36 +0800

    字符串字符词频统计

--- src/main/java/com/xiaodai/algorithm/StringUtil.java
+++ src/main/java/com/xiaodai/algorithm/StringUtil.java
@@ -85,7 +85,34 @@ public class StringUtil {
     }
 
 
+    /**
+     * 3、返回一个字符串的字符统计串
+     * @param str
+     * @return
+     */
+    public String getCountString(String str) {
+        if(str == null || str.equals("")) {
+            return "";
+        }
 
+        char[] chars = str.toCharArray();
+        String res = String.valueOf(chars[0]);
+        int num = 1;
+        for (int i = 1; i < chars.length; i++) {
+            // 结算
+            if(chars[i] != chars[i-1]) {
+                res = concat(res, String.valueOf(num), String.valueOf(chars[i]));
+                num = 1;
+            } else {
+                num++;
+            }
+        }
+        return concat(res, String.valueOf(num), "");
+    }
+
+    private String concat(String s1, String s2, String s3) {
+        return s1 + "_" + s2 + (s3.equals("") ? s3 : "_" + s3);
+    }
 
 
     public static void main(String[] args) {
